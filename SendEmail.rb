095c1a2a9205cf384e04908b452a904faa3a605a
Blag <atejada@gmail.com>
2022-04-08 06:20:33 -0400

    Create SendEmail.rb

---
+++ SendEmail.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# Import your dependencies
+require 'dotenv/load'
+require 'nylas'
+
+# Initialize your Nylas API client
+nylas = Nylas::API.new(
+    app_id: ENV["CLIENT_ID"],
+    app_secret: ENV["CLIENT_SECRET"],
+    access_token: ENV["ACCESS_TOKEN"]
+)
+
+begin
+# Send your email
+	message = nylas.send!(to: [{ email: ENV["RECIPIENT_ADDRESS"], name: "Recipient name" }],
+										subject: "With Love, from Nylas", body: "Well well well...")
+	puts "Message \"#{message.subject}\" was sent with ID #{message.id}"	
+rescue => error
+# Something went wrong
+	puts error.message
+end
